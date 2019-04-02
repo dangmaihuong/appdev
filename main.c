8691d5ac40cec6598a33245ef1e787c2805a06f7
dangmaihuong <dangmaihuong.study@gmail.com>
2019-04-02 09:27:56 +0100

    modified main.c makefile for recording

--- main.c
+++ main.c
@@ -1,16 +1,37 @@
 #include <stdlib.h>	//for random numbers
 #include "screen.h"
-#include <time.h>
-#include<stdio.h>
+#include <time.h>	// for randomrization using system time
+#include <stdio.h>
+#include "sound.h"
 
 int main(){
-	int dec[COL];		//80 pieces of sound decibels
+	FILE *f = fopen("test.wav", "r");
+	short sd[80000];
+	for(;;){ 	//if there is no condition in for so for is true
+		system(RCMD);
+		f = fopen("test.wav", "r");
+		if(f == NULL){
+			printf("Cannot open the file\n");
+			return 1;
+		}
+
+/*	int dec[COL];		//80 pieces of sound decibels
 	srand(time(NULL));
 	for(int i=0; i<COL; i++)	dec[i]=rand()%70+30;	//from 32-99
-	clearScreen();
-	setColors(BLACK, bg(WHITE));
-	barChart(dec);
-//	printf("Message from main\n");
+*/
+		clearScreen();
+		setColors(CYAN, bg(WHITE));
+/*	barChart(dec)
+	printf("Message from main\n");
+*/
+		struct WAVHDR hdr;
+		fread(&hdr, sizeof(hdr), 1, f);		//read WAV header
+		fread(&sd, sizeof(sd), 1, f);		//read WAV data
+//		displayWAVdata
+		fclose(f);
+		displayWAVHDR(hdr);
+	}
+
 	resetColors();
 	getchar();
 //	printf("Message from main\n");
